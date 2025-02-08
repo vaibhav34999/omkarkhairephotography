33cc8043c8030d9dd8a1a74d8a0b7e48928aec99
Vaibhav Pardhi <vaibhav.v.pardhi3499@gmail.com>
2025-02-08 15:24:27 +0530

    developing:

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import "./header.scss";
-
+import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <header>
+      <div>
+        <div>
+            <h1><NavLink to="/" exact activeClassName="active">Home</NavLink></h1>
+        </div>
+        <nav>
+            <ul>
+                <li><NavLink to="/" exact activeClassName="active">Home</NavLink></li>
+                <li><NavLink to="/about" activeClassName="active">About Me</NavLink></li>
+                <li><NavLink to="/portfolio" activeClassName="active">Portfolio</NavLink></li>
+                <li><NavLink to="/services" activeClassName="active">Services</NavLink></li>
+            </ul>
+        </nav>
+        <button><NavLink to="/contact" activeClassName="active">Contact Me</NavLink></button>
+      </div>
+    </header>
   )
 }
 
---
+++ src/screens/Services/Services.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './services.scss';
+
+const Services = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Services
