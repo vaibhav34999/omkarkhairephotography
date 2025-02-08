9a6cb5421a8b45f62af6ca21038218a83f83fbc0
Vaibhav Pardhi <vaibhav.v.pardhi3499@gmail.com>
2025-02-08 17:55:32 +0530

    developing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter as Router } from "react-router-dom";
 import OmkarKhaire from './OmkarKhaire';
